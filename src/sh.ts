5eee9fa2e419b7341426f08b09083206d957236e
Zebreus <lennarteichhorn@googlemail.com>
2022-09-01 14:22:06 +0200

    Add function to execute shell command

---
+++ src/sh.ts
@@ -0,0 +1,18 @@
+import { exec } from "child_process"
+
+export const sh = async (cmd: string) => {
+  return new Promise<{ stdout: string; stderr: string }>(function (resolve, reject) {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        const error = new Error("Failed to create package")
+        err.stack = undefined
+        //@ts-expect-error: we're adding a property to the error
+        error.details = { ...err, stderr, stdout }
+        error.stack = undefined
+        reject(error)
+      } else {
+        resolve({ stdout, stderr })
+      }
+    })
+  })
+}
