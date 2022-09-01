5b05af5d14b263f061c90fef913c237e17f72807
Zebreus <lennarteichhorn@googlemail.com>
2022-09-01 17:56:10 +0200

    Implement test entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
+import { ensureGitRepo } from "helpers/ensureGitRepo"
+import { prepareTargetDir } from "helpers/prepareTargetDir"
+import { normalize, resolve } from "path"
+import { addNixShell } from "steps/addNixShell"
+import { initializeProject } from "steps/initializeProject"
+
 export type Options = {
-  path?: string
-  name?: string
+  path: string
+  name: string
   description?: string
-  type?: "library" | "application"
-  monorepo?: boolean
-  repo?: string
+  type: "library" | "application"
+  monorepo: boolean
+  repo: string
   branch?: string
   authorName?: string
   authorEmail?: string
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const createTypescriptThing = async (settings: Options) => {}
+export const createTypescriptThing = async ({
+  path,
+  name,
+  description,
+  //   type,
+  //   monorepo = false,
+  repo,
+  branch = "main",
+  authorName,
+  authorEmail,
+}: Options) => {
+  const targetDir = normalize(resolve(process.cwd(), path || name || "."))
+  await prepareTargetDir(targetDir)
+  await ensureGitRepo(targetDir, repo, branch)
+  await addNixShell(targetDir)
+  await initializeProject(targetDir, name, "0.0.0", description, authorName, authorEmail, "MIT")
+}
 
 export default createTypescriptThing
