64dbaad89f29ad1cd072310aa1d7f161f4ca81c1
Zebreus <lennarteichhorn@googlemail.com>
2022-09-01 14:21:48 +0200

    Add entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-export const add = (a: number, b: number) => a + b
-export const subtract = (a: number, b: number) => a + b
+export type Options = {
+  path?: string
+  name?: string
+  description?: string
+  type?: "library" | "application"
+  monorepo?: boolean
+  repo?: string
+  branch?: string
+  authorName?: string
+  authorEmail?: string
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const createTypescriptThing = async (settings: Options) => {}
+
+export default createTypescriptThing
