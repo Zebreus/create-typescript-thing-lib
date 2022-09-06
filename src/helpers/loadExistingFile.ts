2f71fe8d6a284cd56b24d123db58c6b42833c03e
Zebreus <lennarteichhorn@googlemail.com>
2022-09-06 12:29:19 +0200

    Fix load existing file

--- src/helpers/loadExistingFile.ts
+++ src/helpers/loadExistingFile.ts
@@ -2,6 +2,10 @@ import { readFile } from "fs/promises"
 import path from "path"
 
 export const loadExistingFile = async (targetDir: string, fileName: string) => {
-  const buffer = await readFile(path.join(targetDir, fileName))
-  return buffer.toString()
+  try {
+    const buffer = await readFile(path.join(targetDir, fileName))
+    return buffer.toString()
+  } catch (e) {
+    return undefined
+  }
 }
