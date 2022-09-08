2e18e2cf8df4cc14e87b2f06b9df88c6dab066db
Zebreus <lennarteichhorn@googlemail.com>
2022-09-08 10:28:36 +0200

    Remove unused resources

--- src/helpers/loadResource.ts
+++
@@ -1,7 +0,0 @@
-import { readFile } from "fs/promises"
-import path from "path"
-
-export const loadResource = async (name: string) => {
-  const buffer = await readFile(path.join(__dirname, "../../resources", name))
-  return buffer.toString()
-}
