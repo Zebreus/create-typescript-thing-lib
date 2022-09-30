b6ec10533442ac0b082462360e6c3876db3665aa
Zebreus <lennarteichhorn@googlemail.com>
2022-09-30 20:27:25 +0200

    Add more error detail in sh

--- src/sh.ts
+++ src/sh.ts
@@ -4,7 +4,7 @@ export const sh = async (cmd: string) => {
   return new Promise<{ stdout: string; stderr: string }>(function (resolve, reject) {
     exec(cmd, (err, stdout, stderr) => {
       if (err) {
-        const error = new Error("Failed to execute command: " + cmd)
+        const error = new Error("Failed to execute command: " + cmd + "\nOutput: " + stdout + "\nError: " + stderr)
         err.stack = undefined
         //@ts-expect-error: we're adding a property to the error
         error.details = { ...err, stderr, stdout }
