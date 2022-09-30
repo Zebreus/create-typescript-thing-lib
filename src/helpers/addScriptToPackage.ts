6a876ca42fdf6efc6e30e1b2d1db2e74dc410c81
Zebreus <lennarteichhorn@googlemail.com>
2022-09-30 21:37:08 +0200

    Fix appending script

--- src/helpers/addScriptToPackage.ts
+++ src/helpers/addScriptToPackage.ts
@@ -32,7 +32,7 @@ export const appendScriptToPackageContent = (packageJsonContent: string, scriptN
     ...packageJson,
     scripts: {
       ...packageJson.scripts,
-      [scriptName]: [packageJson?.scripts?.scriptName ?? [], script].join(" && "),
+      [scriptName]: [packageJson?.scripts?.[scriptName], script].filter(v => v).join(" && "),
     },
   }
   return JSON.stringify(newPackage, null, 2)
