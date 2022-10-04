289f2bd4d673b39caec50af27cddd78fce71dae5
Zebreus <lennarteichhorn@googlemail.com>
2022-10-04 13:27:51 +0200

    Try to create another test for updating

--- src/tests/update.test.ts
+++ src/tests/update.test.ts
@@ -1,4 +1,6 @@
 import fs from "fs"
+import { commitWithAuthor } from "helpers/commitWithAuthor"
+import { augmentJsonConfig } from "helpers/modifyJsonFile"
 import { resolveRef } from "isomorphic-git"
 import { runInDirectory } from "tests/runInDirectory"
 
@@ -23,6 +25,44 @@ describe("Tests for updating repos", () => {
             expect(commitBeforeUpdate).toBeDefined()
             expect(commitAfterUpdate).toBeDefined()
             expect(commitBeforeUpdate).toBe(commitAfterUpdate)
+          }
+        )
+      }
+    )
+  })
+
+  test.concurrent("Updating does update the version", async () => {
+    await runInDirectory(
+      {
+        type: "library",
+      },
+      async source => {
+        const dummyConfig = {
+          targetDir: source,
+          packageManager: "npm",
+          gitCommits: true,
+          gitRepo: true,
+          logger: {
+            logMessage: () => {},
+            logState: () => {},
+          },
+        } as const
+        await augmentJsonConfig(dummyConfig, "package.json", { devDependencies: { eslint: "0.0.0" } })
+        await commitWithAuthor(dummyConfig, "Downgrade eslint")
+
+        await runInDirectory(
+          {
+            type: "library",
+            updateFrom: source,
+          },
+          async updatedDirectory => {
+            const commitBeforeUpdate = await resolveRef({ fs, dir: source, ref: "HEAD" }).catch(() => undefined)
+            const commitAfterUpdate = await resolveRef({ fs, dir: updatedDirectory, ref: "HEAD" }).catch(
+              () => undefined
+            )
+            expect(commitBeforeUpdate).toBeDefined()
+            expect(commitAfterUpdate).toBeDefined()
+            expect(commitBeforeUpdate).not.toBe(commitAfterUpdate)
           }
         )
       }
